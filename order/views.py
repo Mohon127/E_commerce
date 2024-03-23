9fc79036045bc0b7c8af2fa1c9a38be08739669b
Mohon127 <s2110976127@ru.ac.bd>
2024-03-23 22:51:09 +0600

    3rd commit

--- order/views.py
+++ order/views.py
@@ -1,10 +1,14 @@
 from django.views import generic
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .form import CheckoutForm
 
 
 class Checkout(generic.View):
+    login_url = reverse_lazy('login')
+
     def get(self, *args, **kwargs):
         form = CheckoutForm()
         context = {
--- product/views.py
+++ product/views.py
@@ -16,6 +16,7 @@ from .models import(
 )
 
 class Home(generic.TemplateView):
+    
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
